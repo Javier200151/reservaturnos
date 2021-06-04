1317242e303caad9746a052fbf5cdccd12b44934
Javier Esteban López Peña <55264059+Javier200151@users.noreply.github.com>
2021-06-04 10:23:12 -0500

    Add files via upload

---
+++ src/main/java/com/asesoftware/reservaturnos/ReservaturnosApplication.java
@@ -0,0 +1,13 @@
+package com.asesoftware.reservaturnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaturnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaturnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asesoftware/reservaturnos/ReservaturnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asesoftware.reservaturnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaturnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
